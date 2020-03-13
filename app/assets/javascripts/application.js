f3538e1b8368926d5d2f519996fa987527b55c60
irene macapinlac <irenekaymac@gmail.com>
2020-03-14 00:51:15 +1100

    changes to css login page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require rails-ujs
 //= require_tree .
 
-= require jquery3
-= require popper
-= require bootstrap-sprockets
+// = require jquery3
+// = require popper
+// = require bootstrap-sprockets
